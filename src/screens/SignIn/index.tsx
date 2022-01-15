109f09c1799dabc21692c5436d3580dd12d08179
Luan Santos <luanlss10@outlook.com>
2022-01-15 03:39:10 -0300

    segundo commit

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react'; { }
 import {
     Text,
     View,
     TextInput,
-    Image
+    Image,
+    StatusBar,
+
 } from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
 import logo from '../../assets/logo.png'
 import { styles } from './styles'
 
@@ -15,23 +17,33 @@ export function SignIn() { // se colocar export default deve ser exportado sem {
 
     return (
         <View style={styles.container}>
-            <Image 
-            source={logo} 
-            style={styles.image}
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Image
+                source={logo}
+                style={styles.image}
             />
 
             <View style={styles.content}>
-                <Text style={styles.title}> 
+                <Text style={styles.title}>
                     Organize {`\n`}
                     sua jogatinas {`\n`}
                     facilmente {`\n`}
                 </Text>
 
                 <Text style={styles.subtitle}>
-                Crie grupos para jogar seus games {`\n`}
-                favoritos com seus amigos
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
                 </Text>
 
+                <ButtonIcon 
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                    />
+                    
             </View>
         </View>
     );
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import {StyleSheet} from 'react-native';
+import {theme} from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
     container : {
         flex : 1,
-        backgroundColor : '#faaf0a',
         alignItems : 'center',
         justifyContent : 'center',
+        backgroundColor: theme.colors.background
         
     },
 
@@ -14,8 +15,25 @@ export const styles = StyleSheet.create({
         width : 50,
         borderBottomWidth : 2,
     },
-
     image : {
-        
+        width: '100%',
+        height: 360,
+    },
+    content : {
+        marginTop : -40,
+        paddingHorizontal: 50
+    },
+    title : {
+        color : theme.colors.heading,
+        textAlign : 'center',
+        fontSize : 40,
+        marginBottom : 16,
+
+    },
+    subtitle : {
+        color: theme.colors.heading,
+        fontSize : 15,
+        textAlign : 'center',
+        marginBottom : 64
     }
 });
